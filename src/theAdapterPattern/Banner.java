b7cf5a0199309161c1ed17e19cbaf6c4f836bcca
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-01-16 23:13:58 +0900

    Bannerのテストを作成した。テスト作成に伴い実装をやや変更した

--- src/theAdapterPattern/Banner.java
+++ src/theAdapterPattern/Banner.java
@@ -6,11 +6,15 @@ public class Banner {
     public Banner(String string) {
         this.string = string;
     }
-    public void showWithParen() {
-        System.out.println("(" + string + ")");
+    public String showWithParen() {
+        String paren = "(" + string + ")";
+        System.out.println(paren);
+        return paren;
     }
-    public void showWithAster() {
-        System.out.println("*" + string + "*");
+    public String showWithAster() {
+        String aster = "*" + string + "*";
+        System.out.println(aster);
+        return aster;
     }
 
 }
