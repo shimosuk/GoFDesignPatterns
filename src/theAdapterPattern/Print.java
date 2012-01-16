7d709c1a74743c0d7e21a197fbe5c16991cb9293
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-01-17 00:06:46 +0900

    PrintBannerがPrintとBannerをつないだ！

---
+++ src/theAdapterPattern/Print.java
@@ -0,0 +1,8 @@
+package theAdapterPattern;
+
+public interface Print {
+
+    public abstract void printWeak();
+    public abstract void printStrong();
+
+}
---
+++ src/theAdapterPattern/PrintBanner.java
@@ -0,0 +1,19 @@
+package theAdapterPattern;
+
+public class PrintBanner extends Banner implements Print{
+
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    @Override
+    public void printWeak() {
+        showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        showWithAster();
+    }
+
+}
