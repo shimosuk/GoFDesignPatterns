6ff0fcbf46f34668dff64f806f3bef3a7140f1b0
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-04-26 22:36:44 +0900

    FactoryMethodパターン MainクラスにJavaDocを追加
    クラスの役割とサンプルプログラムの説明を記述

--- src/the/factory/method/pattern/Main.java
+++ src/the/factory/method/pattern/Main.java
@@ -1,5 +1,11 @@
 package the.factory.method.pattern;
 
+/**
+ * <h1>動的テストのためのクラス</h1>
+ * <p>このサンプルプログラムは、身分証明書カードを作る工場を題材としている</p>
+ * @author shimosuk
+ *
+ */
 public class Main {
 
     public static void main(String[] args) {
