47e4131cc22fb98d6c293026bae5edb5fc3b4153
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-04-26 23:31:42 +0900

    FactoryMethodパターン Factoryクラスの作成
    TemplateMethodパターンを使ってインスタンスを生成するのが特徴

--- src/the/factory/method/pattern/framework/Factory.java
+++ src/the/factory/method/pattern/framework/Factory.java
@@ -1,5 +1,38 @@
 package the.factory.method.pattern.framework;
 
+/**
+ * <h1>工場を表すクラス</h1>
+ * <p>FactoryMethodパターンの心臓部で、TemplateMethodパターンが使用されている。<p>
+ * @author shimosuk
+ *
+ */
 public abstract class Factory {
 
+    /**
+     * <h1>製品を作って登録するメソッド<h1>
+     * <p>Productクラスのインスタンスを生成する</p>
+     * <p>abstractクラスで実装している(FactoryMethodパターン)</p>
+     * @param owner
+     * @return
+     */
+    public final Product create(String owner) {
+        Product product = createProduct(owner);
+        registerProduct(product);
+        return product;
+    }
+
+    /**
+     * <h1>製品を作るためのメソッド</h1>
+     * <p>抽象メソッドで実装され、createメソッドで呼び出される</p>
+     * @param owner
+     * @return
+     */
+    protected abstract Product createProduct(String owner);
+
+    /**
+     * <h1>製品登録のためのメソッド</h1>
+     * <p>抽象メソッドで実装され、createメソッドで呼び出される</p>
+     * @param product
+     */
+    protected abstract void registerProduct(Product product);
 }
--- src/the/factory/method/pattern/idcard/IDCardFactory.java
+++ src/the/factory/method/pattern/idcard/IDCardFactory.java
@@ -1,7 +1,17 @@
 package the.factory.method.pattern.idcard;
 
 import the.factory.method.pattern.framework.Factory;
+import the.factory.method.pattern.framework.Product;
 
 public class IDCardFactory extends Factory {
+
+    @Override
+    protected Product createProduct(String owner) {
+        return null;
+    }
+
+    @Override
+    protected void registerProduct(Product product) {
+    }
 
 }
